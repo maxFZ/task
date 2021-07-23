e4bfdfa07cdbb1ca036c4cc556269563f7f99e03
maxFZ <maksym2234@gmail.com>
2021-07-23 19:54:00 +0300

    added logging to files

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,20 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppClusterService } from './app-cluster.service';
 import { AppModule } from './app.module';
+import MyLogger from './finder/logger';
+
 
 async function bootstrap() {
-  const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule);
+  // const logger = new Logger('bootstrap');
+  const app = await NestFactory.create(AppModule, {
+    logger: new MyLogger(['log', 'error', 'warn', 'debug', 'verbose']),
+  });
+
+  // const app = await NestFactory.create(AppModule);
 
   const port = process.env.PORT;
   await app.listen(port);
-  logger.log(`Application is running on port ${port}`)
+  // logger.log(`Application is running on port ${port}`)
 }
 // bootstrap();
 
